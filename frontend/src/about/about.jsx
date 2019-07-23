57adea91b7204b1e3e30e5db1350879e6b5e2434
elton-develcode <elton.develcode@develcode.com.br>
2019-07-23 14:37:12 -0300

    teste

--- frontend/src/about/about.jsx
+++ frontend/src/about/about.jsx
@@ -1,7 +1,26 @@
 import React from 'react'
+import PageHeader from '../template/pageHeader'
+
+function paragrafo() {
+  var p = `Lorem ipsum dolor sit amet consectetur adipisicing elit.
+  Asperiores ipsa error praesentium doloremque aliquid
+  recusandae architecto illo officia, deleniti temporibus
+  dignissimos impedit nisi officiis eius voluptates totam
+  deserunt nostrum accusantium.`
+
+  return p
+}
 
 export default props => (
   <div>
-    <h1>Sobre</h1>
+    <PageHeader name='Sobre' small='Nós' />
+    <h2>Nossa História</h2>
+    <p>{paragrafo()}</p>
+    
+    <h2>Missão e Visão</h2>
+    <p>{paragrafo()}</p>
+
+    <h2>Impressa</h2>
+    <p>{paragrafo()}</p>
   </div>
 )
