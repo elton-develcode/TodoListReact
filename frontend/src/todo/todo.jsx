f05e4245c6c4c880076d9084b1e91821f3988dbe
elton-develcode <elton.develcode@develcode.com.br>
2019-07-24 09:21:15 -0300

    Terminando o segundo projeto

--- frontend/src/todo/todo.jsx
+++ frontend/src/todo/todo.jsx
@@ -17,6 +17,7 @@ export default class Todo extends Component {
     this.handleMarkAsDone = this.handleMarkAsDone.bind(this)
     this.handleMarkAsPending = this.handleMarkAsPending.bind(this)
     this.handleSearch = this.handleSearch.bind(this)
+    this.handleClear = this.handleClear.bind(this)
     this.refresh()
   }
   
@@ -60,6 +61,11 @@ export default class Todo extends Component {
       this.refresh(this.state.description)
   }
 
+  handleClear() {
+    console.log('passei aqui')
+    this.refresh()
+  }
+
   render() {
     return (
       <div>
@@ -69,7 +75,8 @@ export default class Todo extends Component {
           description={this.state.description}
           handleChange={this.handleChange}
           handleAdd={this.handleAdd}
-          handleSearch={this.handleSearch} />
+          handleSearch={this.handleSearch}
+          handleClear={this.handleClear} />
         
         <TodoList
             list={this.state.list}
